d5bdc8da37a283494fb10ce5de4b29946e89550c
webvtec <webvtec8@gmail.com>
2025-06-18 16:04:23 -0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,11 @@
 // Replace these with your Firebase project details
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyA-qvC7FrUt1yrjEqhN7Paw648j4ISWVgM",
+  authDomain: "resturantsite-a2d56.firebaseapp.com",
+  projectId: "resturantsite-a2d56",
+  storageBucket: "resturantsite-a2d56.firebasestorage.app",
+  messagingSenderId: "492861664245",
+  appId: "1:492861664245:web:825a8474f14647e1a8cebc"
 };
 
 firebase.initializeApp(firebaseConfig);
